a98ba3832af7307428d38c0c8663944603c152be
Anand Suresh <wwwanandsuresh@gmail.com>
2019-12-14 01:51:16 +0530

    changed the sign

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def index():
     if address is not None:
         fplot, best_improvement = get_plot(address)
         folium_html = fplot._repr_html_()
-    return render_template('index.html', title='Home', folium=folium_html, best_improvement=best_improvement * 100)
+    return render_template('index.html', title='Home', folium=folium_html, best_improvement=best_improvement * -100)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
